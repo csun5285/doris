csun5285/doris

commit bbcfb78458be0ffc8ac4e54c49567f600c3cc1db

(selectdb-cloud) Fix coredump caused by segment downloaded with s3 transfer manager (#1804)
Fix issue http://39.106.86.136:8090/browse/CORE-2063

be/src/cloud/io/cloud_file_segment.cpp
@@ -222,8 +222,14 @@ FileSegment::~FileSegment() {
     }
 }
 
-Status FileSegment::finalize_write() {
+Status FileSegment::finalize_write(bool need_to_get_file_size) {
+    size_t downloaded_size = 0;
+    if (need_to_get_file_size) {
+        std::string file_path = get_path_in_local_cache(true);
+        RETURN_IF_ERROR(global_local_filesystem()->file_size(file_path, &downloaded_size));
+    }
     std::lock_guard segment_lock(_mutex);
+    _downloaded_size = downloaded_size;
     if (_downloaded_size != _segment_range.size()) {
         std::lock_guard cache_lock(_cache->_mutex);
         size_t old_size = _segment_range.size();
@@ -265,15 +271,15 @@ Status FileSegment::set_downloaded(std::lock_guard<doris::Mutex>& /* segment_loc
 
     if (_cache_writer) {
         RETURN_IF_ERROR(_cache_writer->close(false));
-        std::error_code ec;
-        std::filesystem::rename(_cache_writer->path(), get_path_in_local_cache(), ec);
-        if (ec) {
-            LOG(ERROR) << fmt::format("failed to rename {} to {} : {}",
-                                      _cache_writer->path().string(), get_path_in_local_cache(),
-                                      ec.message());
-            status = Status::IOError(ec.message());
-        }
         _cache_writer.reset();
+    }
+
+    std::error_code ec;
+    std::filesystem::rename(get_path_in_local_cache(true), get_path_in_local_cache(), ec);
+    if (ec) {
+        LOG(ERROR) << fmt::format("failed to rename {} to {} : {}", get_path_in_local_cache(true),
+                                  get_path_in_local_cache(), ec.message());
+        status = Status::IOError(ec.message());
     }
 
     if (status) [[likely]] {

be/src/cloud/io/cloud_file_cache_downloader.cpp
@@ -110,7 +110,10 @@ void FileCacheSegmentS3Downloader::download_segments(DownloadTask task) {
                                 return; // not finish
                             }
                             if (handle->GetStatus() == Aws::Transfer::TransferStatus::COMPLETED) {
-                                file_segment->finalize_write();
+                                Status st = file_segment->finalize_write(true);
+                                if (!st) {
+                                    LOG(WARNING) << "s3 download error " << st;
+                                }
                                 file_cache_downloader_counter << file_segment->range().size();
                             } else {
                                 LOG(WARNING) << "s3 download error " << handle->GetStatus();
@@ -121,7 +124,7 @@ void FileCacheSegmentS3Downloader::download_segments(DownloadTask task) {
                                 _inflight_tasks.erase(meta.tablet_id());
                             }
                         };
-                std::string download_file = file_segment->get_path_in_local_cache();
+                std::string download_file = file_segment->get_path_in_local_cache(true);
                 auto createFileFn = [=]() {
                     return Aws::New<Aws::FStream>(meta.file_name().c_str(), download_file.c_str(),
                                                   std::ios_base::out | std::ios_base::in |

be/src/cloud/io/cloud_file_segment.h
@@ -86,7 +86,7 @@ public:
     Status read_at(Slice buffer, size_t offset);
 
     // finish write, release the file writer
-    Status finalize_write();
+    Status finalize_write(bool need_to_get_file_size = false);
 
     // set downloader if state == EMPTY
     uint64_t get_or_set_downloader();

commit d9fe1069a18bf3449b9ff65973b7905629665b37

(selectdb-cloud) Fix bug introduced by #1804

be/src/cloud/io/cloud_file_segment.cpp
@@ -229,7 +229,7 @@ Status FileSegment::finalize_write(bool need_to_get_file_size) {
         RETURN_IF_ERROR(global_local_filesystem()->file_size(file_path, &downloaded_size));
     }
     std::lock_guard segment_lock(_mutex);
-    _downloaded_size = downloaded_size;
+    _downloaded_size = need_to_get_file_size ? downloaded_size : _downloaded_size;
     if (_downloaded_size != _segment_range.size()) {
         std::lock_guard cache_lock(_cache->_mutex);
         size_t old_size = _segment_range.size();