csun5285/doris

commit 5a71a58ec33e14cd099e449ed7451c8955483c6f

(selectdb-cloud) Lazy-open necessary partitions when load (#1766)
The previous implementation will write empty rowsets even if there is no data in partitions.
It will lead to unnecessary requests to MS and object storage, and cause unnecessary resource usage.

To enable this improvement, (it is false by default)
```
enable_lazy_open_partition = true
```

be/src/runtime/tablets_channel.h
@@ -18,6 +18,7 @@
 #pragma once
 
 #include <cstdint>
+#include <mutex>
 #include <unordered_map>
 #include <utility>
 #include <vector>
@@ -69,6 +70,9 @@ public:
 
     Status open(const PTabletWriterOpenRequest& request);
 
+    // Open specific partition all writers
+    Status open_all_writers_for_partition(const OpenPartitionRequest& request);
+
     // no-op when this channel has been closed or cancelled
     template <typename TabletWriterAddRequest, typename TabletWriterAddResult>
     Status add_batch(const TabletWriterAddRequest& request, TabletWriterAddResult* response);
@@ -98,6 +102,10 @@ private:
     // open all writer
     Status _open_all_writers(const PTabletWriterOpenRequest& request);
 
+    template <typename TabletWriterAddRequest>
+    Status _open_all_writers_for_partition(int64_t tablet_id,
+                                           const TabletWriterAddRequest& request);
+
     bool _try_to_wait_flushing();
 
 #ifndef CLOUD_MODE
@@ -108,6 +116,7 @@ private:
                      PSlaveTabletNodes slave_tablet_nodes, const bool write_single_replica);
 #endif
 
+    void _build_partition_tablets_relation(const PTabletWriterOpenRequest& request);
     void _add_broken_tablet(int64_t tablet_id);
     bool _is_broken_tablet(int64_t tablet_id);
 
@@ -118,6 +127,7 @@ private:
     doris::Mutex _lock;
 
     SpinLock _tablet_writers_lock;
+    std::mutex _tablet_writers_open_lock;
 
     enum State {
         kInitialized,
@@ -145,8 +155,13 @@ private:
     // currently it's OK.
     Status _close_status;
 
+    std::map<int64, std::vector<int64>> _partition_tablets_map;
+    std::map<int64, int64> _tablet_partition_map;
+
     // tablet_id -> TabletChannel
     std::unordered_map<int64_t, DeltaWriter*> _tablet_writers;
+    SpinLock _tablet_writers_init_lock;
+    bool _tablet_writers_init = false;
     // broken tablet ids.
     // If a tablet write fails, it's id will be added to this set.
     // So that following batch will not handle this tablet anymore.

be/src/common/config.h
@@ -419,6 +419,9 @@ CONF_mInt32(streaming_load_rpc_max_alive_time_sec, "1200");
 // the timeout of a rpc to open the tablet writer in remote BE.
 // short operation time, can set a short timeout
 CONF_Int32(tablet_writer_open_rpc_timeout_sec, "60");
+// The configuration is used to enable lazy open feature, and the default value is false.
+// When there is mixed deployment in the upgraded version, it needs to be set to false.
+CONF_Bool(enable_lazy_open_partition, "false");
 // You can ignore brpc error '[E1011]The server is overcrowded' when writing data.
 CONF_mBool(tablet_writer_ignore_eovercrowded, "false");
 CONF_mInt32(slave_replica_writer_rpc_timeout_sec, "60");

be/src/exec/tablet_sink.cpp
@@ -45,6 +45,40 @@
 namespace doris {
 namespace stream_load {
 
+class OpenPartitionClosure : public google::protobuf::Closure {
+public:
+    OpenPartitionClosure(NodeChannel* node_channel, IndexChannel* index_channel,
+                         int64_t partition_id)
+            : node_channel(node_channel),
+              index_channel(index_channel),
+              partition_id(partition_id) {};
+
+    ~OpenPartitionClosure() = default;
+
+    void Run() override {
+        SCOPED_SWITCH_THREAD_MEM_TRACKER_LIMITER(ExecEnv::GetInstance()->orphan_mem_tracker());
+        if (cntl.Failed()) {
+            std::stringstream ss;
+            ss << "failed to open partition, error=" << berror(this->cntl.ErrorCode())
+               << ", error_text=" << this->cntl.ErrorText();
+            LOG(WARNING) << ss.str() << " " << node_channel->channel_info();
+            node_channel->cancel("Open partition error");
+            index_channel->mark_as_failed(node_channel->node_id(), node_channel->host(),
+                                          fmt::format("{}, open failed, err: {}",
+                                                      node_channel->channel_info(), ss.str()),
+                                          -1);
+        }
+    }
+
+    void join() { brpc::Join(cntl.call_id()); }
+
+    brpc::Controller cntl;
+    OpenPartitionResult result;
+    NodeChannel* node_channel;
+    IndexChannel* index_channel;
+    int64_t partition_id;
+};
+
 NodeChannel::NodeChannel(OlapTableSink* parent, IndexChannel* index_channel, int64_t node_id)
         : _parent(parent), _index_channel(index_channel), _node_id(node_id) {
     _node_channel_tracker = std::make_shared<MemTracker>(fmt::format(
@@ -152,6 +186,34 @@ void NodeChannel::open() {
                               _open_closure);
     request.release_id();
     request.release_schema();
+}
+
+void NodeChannel::open_partition(int64_t partition_id) {
+    _timeout_watch.reset();
+    SCOPED_CONSUME_MEM_TRACKER(_node_channel_tracker.get());
+    OpenPartitionRequest request;
+    *request.mutable_id() = _parent->_load_id;
+    request.set_index_id(_index_channel->_index_id);
+    for (auto& tablet : _all_tablets) {
+        if (partition_id == tablet.partition_id) {
+            auto ptablet = request.add_tablets();
+            ptablet->set_partition_id(tablet.partition_id);
+            ptablet->set_tablet_id(tablet.tablet_id);
+        }
+    }
+
+    auto open_partition_closure =
+            std::make_unique<OpenPartitionClosure>(this, _index_channel, partition_id);
+
+    int remain_ms = _rpc_timeout_ms - _timeout_watch.elapsed_time();
+    if (UNLIKELY(remain_ms < config::min_load_rpc_timeout_ms)) {
+        remain_ms = config::min_load_rpc_timeout_ms;
+    }
+    open_partition_closure->cntl.set_timeout_ms(remain_ms);
+    open_partition_closure->cntl.set_timeout_ms(remain_ms);
+    _stub->open_partition(&open_partition_closure.get()->cntl, &request,
+                          &open_partition_closure.get()->result, open_partition_closure.get());
+    _open_partition_closures.insert(std::move(open_partition_closure));
 }
 
 void NodeChannel::_cancel_with_msg(const std::string& msg) {
@@ -429,52 +491,6 @@ void NodeChannel::_close_check() {
     std::lock_guard<std::mutex> lg(_pending_batches_lock);
     CHECK(_pending_batches.empty()) << name();
     CHECK(_cur_batch == nullptr) << name();
-}
-Status NodeChannel::close_wait(RuntimeState* state) {
-    SCOPED_CONSUME_MEM_TRACKER(_node_channel_tracker.get());
-    // set _is_closed to true finally
-    Defer set_closed {[&]() {
-        std::lock_guard<std::mutex> l(_closed_lock);
-        _is_closed = true;
-    }};
-
-    auto st = none_of({_cancelled, !_eos_is_produced});
-    if (!st.ok()) {
-        if (_cancelled) {
-            std::lock_guard<SpinLock> l(_cancel_msg_lock);
-            return Status::InternalError("wait close failed. {}", _cancel_msg);
-        } else {
-            return std::move(
-                    st.prepend("already stopped, skip waiting for close. cancelled/!eos: "));
-        }
-    }
-
-    // waiting for finished, it may take a long time, so we couldn't set a timeout
-    while (!_add_batches_finished && !_cancelled && !state->is_cancelled()) {
-        bthread_usleep(1000);
-    }
-    _close_time_ms = UnixMillis() - _close_time_ms;
-
-    if (_add_batches_finished) {
-        _close_check();
-        state->tablet_commit_infos().insert(state->tablet_commit_infos().end(),
-                                            std::make_move_iterator(_tablet_commit_infos.begin()),
-                                            std::make_move_iterator(_tablet_commit_infos.end()));
-
-        _index_channel->set_error_tablet_in_state(state);
-        _index_channel->set_tablets_received_rows(_tablets_received_rows, _node_id);
-        return Status::OK();
-    }
-
-    std::stringstream ss;
-    ss << "close wait failed coz rpc error";
-    {
-        std::lock_guard<SpinLock> l(_cancel_msg_lock);
-        if (_cancel_msg != "") {
-            ss << ". " << _cancel_msg;
-        }
-    }
-    return Status::InternalError(ss.str());
 }
 
 void NodeChannel::cancel(const std::string& cancel_msg) {
@@ -1024,10 +1040,13 @@ Status OlapTableSink::open(RuntimeState* state) {
         RETURN_IF_ERROR(Expr::open(_output_expr_ctxs, state));
     }
 
+    fmt::memory_buffer buf;
     for (auto index_channel : _channels) {
+        fmt::format_to(buf, "index id:{}", index_channel->index_id());
         index_channel->for_each_node_channel(
                 [](const std::shared_ptr<NodeChannel>& ch) { ch->open(); });
     }
+    VLOG_DEBUG << "list of open index id = " << fmt::to_string(buf);
 
     for (auto index_channel : _channels) {
         index_channel->for_each_node_channel([&index_channel](
@@ -1277,6 +1296,56 @@ Status OlapTableSink::close(RuntimeState* state, Status close_status) {
     _close_status = status;
     DataSink::close(state, close_status);
     return status;
+}
+
+Status NodeChannel::close_wait(RuntimeState* state) {
+    SCOPED_CONSUME_MEM_TRACKER(_node_channel_tracker.get());
+    for (auto& open_partition_closure : _open_partition_closures) {
+        open_partition_closure->join();
+    }
+    // set _is_closed to true finally
+    Defer set_closed {[&]() {
+        std::lock_guard<std::mutex> l(_closed_lock);
+        _is_closed = true;
+    }};
+
+    auto st = none_of({_cancelled, !_eos_is_produced});
+    if (!st.ok()) {
+        if (_cancelled) {
+            std::lock_guard<SpinLock> l(_cancel_msg_lock);
+            return Status::InternalError("wait close failed. {}", _cancel_msg);
+        } else {
+            return std::move(
+                    st.prepend("already stopped, skip waiting for close. cancelled/!eos: "));
+        }
+    }
+
+    // waiting for finished, it may take a long time, so we couldn't set a timeout
+    while (!_add_batches_finished && !_cancelled && !state->is_cancelled()) {
+        bthread_usleep(1000);
+    }
+    _close_time_ms = UnixMillis() - _close_time_ms;
+
+    if (_add_batches_finished) {
+        _close_check();
+        state->tablet_commit_infos().insert(state->tablet_commit_infos().end(),
+                                            std::make_move_iterator(_tablet_commit_infos.begin()),
+                                            std::make_move_iterator(_tablet_commit_infos.end()));
+
+        _index_channel->set_error_tablet_in_state(state);
+        _index_channel->set_tablets_received_rows(_tablets_received_rows, _node_id);
+        return Status::OK();
+    }
+
+    std::stringstream ss;
+    ss << "close wait failed coz rpc error";
+    {
+        std::lock_guard<SpinLock> l(_cancel_msg_lock);
+        if (_cancel_msg != "") {
+            ss << ". " << _cancel_msg;
+        }
+    }
+    return Status::InternalError(ss.str());
 }
 
 Status OlapTableSink::_convert_batch(RuntimeState* state, RowBatch* input_batch,

be/src/exec/tablet_sink.h
@@ -60,6 +60,7 @@ class MutableBlock;
 namespace stream_load {
 
 class OlapTableSink;
+class OpenPartitionClosure;
 
 // The counter of add_batch rpc of a single node
 struct AddBatchCounter {
@@ -183,6 +184,9 @@ public:
 
     // we use open/open_wait to parallel
     void open();
+
+    virtual void open_partition(int64_t partition_id);
+
     virtual Status open_wait();
 
     Status add_row(Tuple* tuple, int64_t tablet_id);
@@ -200,7 +204,8 @@ public:
     // 1. mark_close()->close_wait() PS. close_wait() will block waiting for the last AddBatch rpc response.
     // 2. just cancel()
     virtual void mark_close();
-    Status close_wait(RuntimeState* state);
+
+    virtual Status close_wait(RuntimeState* state);
 
     void cancel(const std::string& cancel_msg);
 
@@ -335,6 +340,7 @@ private:
     using AddBatchReq = std::pair<std::unique_ptr<RowBatch>, PTabletWriterAddBatchRequest>;
     std::queue<AddBatchReq> _pending_batches;
     ReusableClosure<PTabletWriterAddBatchResult>* _add_batch_closure = nullptr;
+    std::unordered_set<std::unique_ptr<OpenPartitionClosure>> _open_partition_closures;
 };
 
 class IndexChannel {

be/src/runtime/load_channel.cpp
@@ -58,7 +58,8 @@ Status LoadChannel::open(const PTabletWriterOpenRequest& params) {
         } else {
             // create a new tablets channel
             TabletsChannelKey key(params.id(), index_id);
-            channel.reset(new TabletsChannel(key, _load_id, _is_high_priority, _is_vec));
+            channel = std::make_shared<TabletsChannel>(key, _load_id, _is_high_priority,
+                                                       _is_vec);
             {
                 std::lock_guard<SpinLock> l(_tablets_channels_lock);
                 _tablets_channels.insert({index_id, channel});
@@ -70,6 +71,36 @@ Status LoadChannel::open(const PTabletWriterOpenRequest& params) {
 
     _opened = true;
     _last_updated_time.store(time(nullptr));
+    return Status::OK();
+}
+
+Status LoadChannel::open_partition(const OpenPartitionRequest& params) {
+    int64_t index_id = params.index_id();
+    // check finish
+    {
+        std::lock_guard<std::mutex> l(_lock);
+        auto it = _finished_channel_ids.find(index_id);
+        if (it != _finished_channel_ids.end()) {
+            return Status::OK();
+        }
+    }
+    std::shared_ptr<TabletsChannel> channel;
+    {
+        std::lock_guard<std::mutex> l(_lock);
+        auto it = _tablets_channels.find(index_id);
+        if (it != _tablets_channels.end()) {
+            channel = it->second;
+        } else {
+            fmt::memory_buffer buf;
+            for (auto tablet : params.tablets()) {
+                fmt::format_to(buf, "tablet id:{}", tablet.tablet_id());
+            }
+            LOG(WARNING) << "should be opened partition index id=" << params.index_id()
+                         << "tablet ids=" << fmt::to_string(buf);
+            return Status::InternalError("Partition should be opened");
+        }
+    }
+    RETURN_IF_ERROR(channel->open_all_writers_for_partition(params));
     return Status::OK();
 }
 

be/src/runtime/load_channel.h
@@ -46,6 +46,8 @@ public:
     // open a new load channel if not exist
     Status open(const PTabletWriterOpenRequest& request);
 
+    Status open_partition(const OpenPartitionRequest& params);
+
     // this batch must belong to a index in one transaction
     template <typename TabletWriterAddRequest, typename TabletWriterAddResult>
     Status add_batch(const TabletWriterAddRequest& request, TabletWriterAddResult* response);

be/src/runtime/load_channel_mgr.cpp
@@ -121,6 +121,17 @@ Status LoadChannelMgr::open(const PTabletWriterOpenRequest& params) {
     return Status::OK();
 }
 
+Status LoadChannelMgr::open_partition(const OpenPartitionRequest& params) {
+    UniqueId load_id(params.id());
+    std::shared_ptr<LoadChannel> channel;
+    auto it = _load_channels.find(load_id);
+    if (it == _load_channels.end()) {
+        return Status::InternalError("unknown load id, load id=" + load_id.to_string());
+    }
+    RETURN_IF_ERROR(it->second->open_partition(params));
+    return Status::OK();
+}
+
 static void dummy_deleter(const CacheKey& key, void* value) {}
 
 void LoadChannelMgr::_finish_load_channel(const UniqueId load_id) {

be/src/runtime/load_channel_mgr.h
@@ -53,6 +53,8 @@ public:
     // open a new load channel if not exist
     Status open(const PTabletWriterOpenRequest& request);
 
+    Status open_partition(const OpenPartitionRequest& params);
+
     template <typename TabletWriterAddRequest, typename TabletWriterAddResult>
     Status add_batch(const TabletWriterAddRequest& request, TabletWriterAddResult* response);
 

commit cd2c4324bf2bfe7530e68c9f7d50d73343a23951

(selectdb-cloud) Fix compile error in tablets_channel.h introduced by #1766 (#1880)
* fix `error: private field '_tablet_writers_init' is not used [-Werror,-Wunused-private-field]`
* refer to `https://github.com/selectdb/selectdb-core/pull/1766`

be/src/runtime/tablets_channel.h
@@ -160,8 +160,7 @@ private:
 
     // tablet_id -> TabletChannel
     std::unordered_map<int64_t, DeltaWriter*> _tablet_writers;
-    SpinLock _tablet_writers_init_lock;
-    bool _tablet_writers_init = false;
+
     // broken tablet ids.
     // If a tablet write fails, it's id will be added to this set.
     // So that following batch will not handle this tablet anymore.