9ddd5afbbdfd06d39fab571b17a85a7e32be4cf7
Lei Zhang <27994433+SWJTU-ZhangLei@users.noreply.github.com>
2023-08-11 16:21:57 +0800

    (cloud-merge-2.0) Fix several schema_change case (#2316)

--- regression-test/suites/cloud/alter_clause_p0/alter_table_column/test_agg_mv_schema_change.groovy
+++ regression-test/suites/cloud/alter_clause_p0/alter_table_column/test_agg_mv_schema_change.groovy
@@ -74,7 +74,7 @@ suite ("test_agg_mv_schema_change") {
 
         //add materialized view
         def mvName = "mv1"
-        sql "create materialized view ${mvName} as select user_id, date, city, age, sum(cost) from ${tableName} group by user_id, date, city, age, sex;"
+        sql "create materialized view ${mvName} as select user_id, date, city, age, sum(cost) from ${tableName} group by user_id, date, city, age;"
         int max_try_secs = 600
         while (max_try_secs--) {
             String result = getMvJobState(tableName)
--- regression-test/suites/schema_change_p0/test_agg_mv_schema_change.groovy
+++ regression-test/suites/schema_change_p0/test_agg_mv_schema_change.groovy
@@ -102,7 +102,7 @@ suite ("test_agg_mv_schema_change") {
         try_sql """ALTER TABLE ${tableName} SET ("light_schema_change" = "true");"""
 
         def mvName2 = "mv2"
-        sql "create materialized view ${mvName2} as select user_id, date, city, cost, max(age) from ${tableName} group by user_id, date, city, cost, sex;"
+        sql "create materialized view ${mvName2} as select user_id, date, city, cost, max(age) from ${tableName} group by user_id, date, city, cost;"
 
         waitForJob(tableName, 3000)
 
