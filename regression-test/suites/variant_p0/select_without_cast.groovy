b7f1b9e54c847eac6d9ae744357fa0b45e05fd61
csun5285 <csun5285@gmail.com>
2023-08-25 22:40:16 +0800

    test

--- regression-test/suites/variant_p0/select_without_cast.groovy
+++ regression-test/suites/variant_p0/select_without_cast.groovy
@@ -25,7 +25,7 @@ suite("regression_test_select_variant_without_cast", "variant_type_select"){
                 v variant
             )
             DUPLICATE KEY(`k`)
-            DISTRIBUTED BY HASH(k) BUCKETS 5
+            DISTRIBUTED BY HASH(k) BUCKETS 1
             properties("replication_num" = "1", "disable_auto_compaction" = "true");
         """
     }
@@ -41,43 +41,37 @@ suite("regression_test_select_variant_without_cast", "variant_type_select"){
         logger.info("update config: code=" + code + ", out=" + out + ", err=" + err)
     }
 
-    try {
-        // select with cast
-        table_name = "select_data"
-        create_table.call(table_name)
-        sql "set enable_two_phase_read_opt = false;"
-        set_be_config.call("ratio_of_defaults_as_sparse_column", "1")
-        sql """insert into  ${table_name} values(10000, '{"a": 3000, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.111}}')"""
-        sql """insert into  ${table_name} values(10001, '{"a": 3000, "b" : "string", "c" : {"c" : 789, "d" : null, "e" : 3.20}}')"""
-        sql """insert into  ${table_name} values(10002, '{"a": 3000, "b" : 2.8, "c" : {"c" : 123, "e" : 3.19}}')"""
-        sql """insert into  ${table_name} values(10003, '{"a": 3500, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.112}}')"""
-        sql """insert into  ${table_name} values(10004, '{"a": 3500, "c" : {"e" : 6.828}}')"""
-        sql """insert into  ${table_name} values(10005, '{"a": 3500, "c" : {"c" : 445}}')"""
-        sql """insert into  ${table_name} values(10006, '{"a": 45000, "b" : [123, {"xx" : 1}], "c" : {"c" : 456, "d" : null, "e" : 7.113}}')"""
-        sql """insert into  ${table_name} values(10007, '{"a": 45000, "c" : {"c" : 59999, "e" : 7.29}}')"""
-        // todo: fix insert error
-        // sql """insert into  ${table_name} values(10008, '{"f" : [1, 2, 3]}')"""
-        sql """insert into  ${table_name} values(10009, '{"a": 45000, "h": "string_1"}')"""
-        sql """insert into  ${table_name} values(10010, '{"a": 45000, "h": "string_2"}')"""
-        qt_sql_1_0 """select cast(v:c.c as int) from ${table_name} order by k"""
-        // todo: fix cast lost info
-        qt_sql_1_1 """select cast(v:b as json) from ${table_name} order by k"""
-        qt_sql_1_2 """select cast(v:c.c as int) from ${table_name} order by cast(v:c.c as int)"""
-        qt_sql_1_3 """select cast(v:c.e as double) from ${table_name} where cast(v:a as int) > 3000 order by k"""
-        // qt_sql_1_4 """select cast(v:f as array) from ${table_name}"""
-        qt_sql_1_5 """select cast(v:c.e as double) from ${table_name} where cast(v:c.e as double) > 7.0 order by k"""
-        qt_sql_1_6 """select sum(cast(v:a as int)) as sum_a from ${table_name} group by cast(v:a as int) order by sum_a"""
-        qt_sql_1_7 """select cast(v:c.e as double) from ${table_name} where cast(v:c.e as double) > 5.0 order by cast(v:c.e as double)"""
-        qt_sql_1_8 """select count(cast(v:c.c as int)) as count_c from ${table_name} group by cast(v:a as int) order by count_c"""
-        qt_sql_1_9 """select max(cast(v:c.e as double)) as max_e from ${table_name} group by cast(v:a as int) order by max_e"""
-        // qt_sql_1_10 """select * from ${table_name} where cast(v:a as int) > 30000 order by k"""
-        // todo: support for delete predicate 
-        // qt_sql_1_11 """delete from ${table_name} where cast(v:a as int) > 30000"""
-        // qt_sql_1_12 """select cast(v:h as string) from ${table_name} where cast(v:a as int) > 30000 order by k"""
-        sql "truncate table ${table_name}"
+    def load_json_data = {table_name, file_name ->
+        // load the json data
+        streamLoad {
+            table "${table_name}"
 
-        // select without cast
-        table_name = "select_data_without_cast"
+            // set http request header params
+            set 'read_json_by_line', 'true' 
+            set 'format', 'json' 
+            set 'max_filter_ratio', '0.1'
+            file file_name // import json file
+            time 10000 // limit inflight 10s
+
+            // if declared a check callback, the default check condition will ignore.
+            // So you must check all condition
+
+            check { result, exception, startTime, endTime ->
+                if (exception != null) {
+                        throw exception
+                }
+                logger.info("Stream load ${file_name} result: ${result}".toString())
+                def json = parseJson(result)
+                assertEquals("success", json.Status.toLowerCase())
+                // assertEquals(json.NumberTotalRows, json.NumberLoadedRows + json.NumberUnselectedRows)
+                assertTrue(json.NumberLoadedRows > 0 && json.LoadBytes > 0)
+            }
+        }
+    }
+
+    try {
+        // simple select
+        table_name = "test_table"
         create_table.call(table_name)
         sql "set enable_two_phase_read_opt = false;"
         set_be_config.call("ratio_of_defaults_as_sparse_column", "1")
@@ -93,7 +87,7 @@ suite("regression_test_select_variant_without_cast", "variant_type_select"){
         sql """insert into  ${table_name} values(10009, '{"a": 45000, "h": "string_1"}')"""
         sql """insert into  ${table_name} values(10010, '{"a": 45000, "h": "string_2"}')"""
         Thread.sleep(12000)
-        qt_sql_2_0 """select v:c.c from ${table_name} order by k"""
+        qt_sql_2 """select v:c.c from ${table_name} order by k"""
         // todo(lhy): fix cast lost info
         qt_sql_2_1 """select v:b from ${table_name} order by k"""
         qt_sql_2_2 """select v:c.c from ${table_name} order by v:c.c"""
@@ -104,31 +98,67 @@ suite("regression_test_select_variant_without_cast", "variant_type_select"){
         qt_sql_2_7 """select v:c.e from ${table_name} where v:c.e > 5.0 order by v:c.e"""
         qt_sql_2_8 """select count(v:c.c) as count_c from ${table_name} group by v:a order by count_c"""
         qt_sql_2_9 """select max(v:c.e) as max_e from ${table_name} group by v:a order by max_e"""
-        qt_sql_2_10 """select * from ${table_name} where v:a >= 30000 order by k"""
+        // todo: fix predicate column type is string
+        // qt_sql_2_10 """select * from ${table_name} where v:h = 'string_2'"""
+        // todo: support for delete predicate 
         // qt_sql_1_11 """delete from ${table_name} where v:a > 30000"""
-        // qt_sql_1_12 """select v:h from ${table_name} where v:a > 30000 order by k"""
+        qt_sql_1_12 """select * from ${table_name} where v:a > 30000 order by k"""
         sql "truncate table ${table_name}"
 
-
+        
         // test where, order, agg ...
-        set_be_config.call("ratio_of_defaults_as_sparse_column", "0.95")
+        // insert 10000 rows
+        set_be_config.call("ratio_of_defaults_as_sparse_column", "1")
         table_name = "github_events"
-        sql """DROP TABLE IF EXISTS ${table_name}"""
-        sql """
-            CREATE TABLE IF NOT EXISTS ${table_name} (
-                k bigint,
-                v variant
-            )
-            DUPLICATE KEY(`k`)
-            DISTRIBUTED BY HASH(k) BUCKETS 4 
-            properties("replication_num" = "1", "disable_auto_compaction" = "false");
-        """
+        create_table.call(table_name)
         // 2022
         load_json_data.call(table_name, """${getS3Url() + '/regression/gharchive.m/2022-11-07-16.json'}""")
-        load_json_data.call(table_name, """${getS3Url() + '/regression/gharchive.m/2022-11-07-10.json'}""")
-        load_json_data.call(table_name, """${getS3Url() + '/regression/gharchive.m/2022-11-07-22.json'}""")
-        load_json_data.call(table_name, """${getS3Url() + '/regression/gharchive.m/2022-11-07-23.json'}""")
         Thread.sleep(12000)
+        sql "set enable_two_phase_read_opt = false;"
+
+        // v:repo.id   int
+        // v:repo.name text
+        // todo: sometime result is empty
+        qt_sql_3 """select v:repo from ${table_name} where v:repo.id = 562172658"""
+        // todo: result is empty, but select v:repo is not
+        qt_sql_3_1 """select v from ${table_name} where v:repo.id = 562172658"""
+        
+        qt_sql_3_2 """select * from ${table_name} order by v:repo.id desc limit 1"""
+        qt_sql_3_3 """select count() from ${table_name} group by v:type"""
+        qt_sql_3_4 """SELECT v:payload.action, count() FROM github_events  GROUP BY v:payload.action"""
+        qt_sql_3_5 """SELECT v:repo.name FROM github_events ORDER BY v:created_at LIMIT 5"""
+        qt_sql_3_6 """SELECT v:repo.name, count() AS stars FROM github_events GROUP BY v:repo.name ORDER BY stars DESC, 1 LIMIT 5"""
+        qt_sql_3_7 """
+            SELECT 
+                concat('https://github.com/', v:repo.name, '/pull/') AS URL,
+                count(distinct v:actor.login) AS authors
+                FROM github_events
+                GROUP BY v:repo.name, v:payload.issue.`number` 
+                ORDER BY authors DESC, URL ASC
+                LIMIT 5
+        """
+        qt_sql_3_8 """
+            SELECT
+                v:repo.name,
+                v:payload.issue.`number`  as number,
+                count() AS comments
+            FROM github_events
+            GROUP BY v:repo.name, number 
+            ORDER BY comments DESC, number ASC, 1
+            LIMIT 5;
+        """
+        qt_sql_3_9 """
+            SELECT
+                lower(split_part(v:repo.name, '/', 1)) AS org,
+                count() AS stars
+            FROM github_events
+            GROUP BY org
+            ORDER BY stars DESC, 1
+            LIMIT 5;
+        """
+        qt_sql_3_10 """
+            SELECT count(distinct v:actor.login) FROM github_events
+        """
 
     } finally {
         // reset flags
