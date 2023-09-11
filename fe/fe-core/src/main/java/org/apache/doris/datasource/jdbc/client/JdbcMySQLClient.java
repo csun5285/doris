csun5285/doris

commit 98f041cfcfd88cbce082f477a67a67bc32be23f2

[fix](jdbc catalog) Add the adaptation SelectDB to jdbc catalog (#2482)

fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcMySQLClient.java
@@ -54,7 +54,7 @@ public class JdbcMySQLClient extends JdbcClient {
             rs = stmt.executeQuery("SHOW VARIABLES LIKE 'version_comment'");
             if (rs.next()) {
                 String versionComment = rs.getString("Value");
-                isDoris = versionComment.toLowerCase().contains("doris");
+                isDoris = versionComment.toLowerCase().contains("doris") || versionComment.toLowerCase().contains("selectdb");
             }
         } catch (SQLException e) {
             throw new JdbcClientException("Failed to determine MySQL Version Comment", e);

commit ac602008db673a02b96ca215327adb4a0ac9f910

(selectdb-cloud) Fix style error introduced by #2482

fe/fe-core/src/main/java/org/apache/doris/datasource/jdbc/client/JdbcMySQLClient.java
@@ -54,7 +54,8 @@ public class JdbcMySQLClient extends JdbcClient {
             rs = stmt.executeQuery("SHOW VARIABLES LIKE 'version_comment'");
             if (rs.next()) {
                 String versionComment = rs.getString("Value");
-                isDoris = versionComment.toLowerCase().contains("doris") || versionComment.toLowerCase().contains("selectdb");
+                isDoris = versionComment.toLowerCase().contains("doris")
+                    || versionComment.toLowerCase().contains("selectdb");
             }
         } catch (SQLException e) {
             throw new JdbcClientException("Failed to determine MySQL Version Comment", e);