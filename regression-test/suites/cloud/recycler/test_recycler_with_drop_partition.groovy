9a9c4a79c71ad1fc158ef0bdee2070b3af6f1abf
Xiaocc <598887962@qq.com>
2023-09-13 14:26:59 +0800

    (selectdb-cloud) Fix data path of recycler cases (#2501)

--- regression-test/suites/cloud/recycler/test_recycler_with_drop_partition.groovy
+++ regression-test/suites/cloud/recycler/test_recycler_with_drop_partition.groovy
@@ -61,7 +61,7 @@ suite("test_recycler_with_drop_partition") {
             set 'columns', columns
             // relate to ${DORIS_HOME}/regression-test/data/demo/streamload_input.csv.
             // also, you can stream load a http stream, e.g. http://xxx/some.csv
-            file """${getS3Url()}/ssb/sf0.1/lineorder.tbl.gz"""
+            file """${getS3Url()}/regression/ssb/sf0.1/lineorder.tbl.gz"""
 
             time 10000 // limit inflight 10s
 
