0e73f8902b26bdfa7b3d474e1450d829790602b9
Luwei <814383175@qq.com>
2023-09-26 21:35:04 +0800

    (selectdb-cloud) Fix null execption of rollup introduced by #2607 (#2609)

--- fe/fe-core/src/main/java/org/apache/doris/alter/RollupJobV2.java
+++ fe/fe-core/src/main/java/org/apache/doris/alter/RollupJobV2.java
@@ -386,7 +386,7 @@ public class RollupJobV2 extends AlterJobV2 implements GsonPostProcessable {
                                 tbl.isInMemory(), tbl.isPersistent(), true, tbl.isDynamicSchema(),
                                 tbl.getName(), tbl.getTTLSeconds(),
                                 tbl.getEnableUniqueKeyMergeOnWrite(), tbl.storeRowColumn(),
-                                tbl.getIndexMetaByIndexId(rollupIndexId).getSchemaVersion());
+                                tbl.getBaseSchemaVersion());
                         requestBuilder.addTabletMetas(builder);
                     } // end for rollupTablets
                     Env.getCurrentInternalCatalog().sendCreateTabletsRpc(requestBuilder);
