csun5285/doris

commit 1920e2f7ad2210efa733234cf8a5b4185d40bfe4

(selectdb-cloud) Fix dead lock when get delete bitmap update lock (#2602)

fe/fe-core/src/main/java/org/apache/doris/transaction/CloudGlobalTransactionMgr.java
@@ -448,9 +448,6 @@ public class CloudGlobalTransactionMgr implements GlobalTransactionMgrInterface 
                     .setLockId(transactionId)
                     .setInitiator(-1)
                     .setExpiration(10);
-            for (Long partitionId : entry.getValue()) {
-                builder.addPartitionIds(partitionId);
-            }
             final GetDeleteBitmapUpdateLockRequest request = builder.build();
             GetDeleteBitmapUpdateLockResponse response = null;
 

be/src/cloud/cloud_meta_mgr.cpp
@@ -48,7 +48,7 @@ static std::string debug_info(const Req& req) {
     } else if constexpr (is_any_v<Req, selectdb::UpdateDeleteBitmapRequest>) {
         return fmt::format(" tablet_id={}, lock_id={}", req.tablet_id(), req.lock_id());
     } else if constexpr (is_any_v<Req, selectdb::GetDeleteBitmapUpdateLockRequest>) {
-        return fmt::format(" partition_id={}, lock_id={}", req.partition_ids(0), req.lock_id());
+        return fmt::format(" table_id={}, lock_id={}", req.table_id(), req.lock_id());
     } else {
         static_assert(!sizeof(Req));
     }
@@ -745,7 +745,6 @@ Status CloudMetaMgr::get_delete_bitmap_update_lock(const Tablet* tablet, int64_t
     selectdb::GetDeleteBitmapUpdateLockResponse res;
     req.set_cloud_unique_id(config::cloud_unique_id);
     req.set_table_id(tablet->table_id());
-    req.add_partition_ids(tablet->partition_id());
     req.set_lock_id(lock_id);
     req.set_initiator(initiator);
     req.set_expiration(10); // 10s expiration time for compaction and schema_change

be/src/cloud/cloud_schema_change.cpp
@@ -17,6 +17,7 @@ namespace doris::cloud {
 using namespace ErrorCode;
 
 static constexpr int ALTER_TABLE_BATCH_SIZE = 4096;
+static constexpr int SCHEMA_CHANGE_DELETE_BITMAP_LOCK_ID = -2;
 
 static std::unique_ptr<SchemaChange> get_sc_procedure(const BlockChanger& changer,
                                                       bool sc_sorting) {
@@ -382,8 +383,8 @@ Status CloudSchemaChange::_process_delete_bitmap(TabletSharedPtr new_tablet, int
     }
 
     // step 3, process incremental rowset with delete bitmap update lock
-    RETURN_IF_ERROR(
-            cloud::meta_mgr()->get_delete_bitmap_update_lock(new_tablet.get(), -2, initiator));
+    RETURN_IF_ERROR(cloud::meta_mgr()->get_delete_bitmap_update_lock(
+            new_tablet.get(), SCHEMA_CHANGE_DELETE_BITMAP_LOCK_ID, initiator));
     RETURN_IF_ERROR(cloud::meta_mgr()->sync_tablet_rowsets(new_tablet.get()));
     int64_t new_max_version = new_tablet->max_version().second;
     LOG(INFO) << "alter table for mow table, calculate delete bitmap of "
@@ -425,8 +426,9 @@ Status CloudSchemaChange::_process_delete_bitmap(TabletSharedPtr new_tablet, int
     new_delete_bitmap->merge(new_tablet->tablet_meta()->delete_bitmap());
 
     // step4, store delete bitmap
-    RETURN_IF_ERROR(cloud::meta_mgr()->update_delete_bitmap(new_tablet.get(), -2, initiator,
-                                                            new_delete_bitmap.get()));
+    RETURN_IF_ERROR(cloud::meta_mgr()->update_delete_bitmap(new_tablet.get(),
+                                                            SCHEMA_CHANGE_DELETE_BITMAP_LOCK_ID,
+                                                            initiator, new_delete_bitmap.get()));
 
     new_tablet->merge_delete_bitmap(*new_delete_bitmap);
     return Status::OK();

be/src/olap/tablet.cpp
@@ -158,6 +158,7 @@ static bvar::PerSecond<bvar::Adder<uint64_t>> g_tablet_pk_not_found_per_second(
         "doris_pk", "lookup_not_found_per_second", &g_tablet_pk_not_found, 60);
 
 constexpr std::chrono::seconds TRACE_TABLET_LOCK_THRESHOLD = 10s;
+static constexpr int COMPACTION_DELETE_BITMAP_LOCK_ID = -1;
 
 DEFINE_COUNTER_METRIC_PROTOTYPE_2ARG(flush_bytes, MetricUnit::BYTES);
 DEFINE_COUNTER_METRIC_PROTOTYPE_2ARG(flush_finish_count, MetricUnit::OPERATIONS);
@@ -3823,8 +3824,8 @@ Status Tablet::update_delete_bitmap(const RowsetSharedPtr& rowset,
         new_delete_bitmap->merge({std::get<0>(iter->first), std::get<1>(iter->first), cur_version},
                                  iter->second);
     }
-    RETURN_IF_ERROR(cloud::meta_mgr()->update_delete_bitmap(this, txn_id, -1,
-                                                   new_delete_bitmap.get()));
+    RETURN_IF_ERROR(cloud::meta_mgr()->update_delete_bitmap(
+            this, txn_id, COMPACTION_DELETE_BITMAP_LOCK_ID, new_delete_bitmap.get()));
 #else
     if (config::enable_merge_on_write_correctness_check && rowset->num_rows() != 0) {
         // only do correctness check if the rowset has at least one row written
@@ -4011,7 +4012,8 @@ Status Tablet::cloud_calc_delete_bitmap_for_compaciton(
     location_map.clear();
 
     // 2. calc delete bimap for incremental data
-    RETURN_IF_ERROR(cloud::meta_mgr()->get_delete_bitmap_update_lock(this, -1, initiator));
+    RETURN_IF_ERROR(cloud::meta_mgr()->get_delete_bitmap_update_lock(
+            this, COMPACTION_DELETE_BITMAP_LOCK_ID, initiator));
     RETURN_IF_ERROR(cloud::meta_mgr()->sync_tablet_rowsets(this));
 
     calc_compaction_output_rowset_delete_bitmap(input_rowsets, rowid_conversion, version.second,

cloud/src/meta-service/keys.h
@@ -146,6 +146,7 @@ using MetaSchemaKeyInfo    = BasicKeyInfo<21, std::tuple<std::string,  int64_t, 
 //                                                      0:instance_id  1:tablet_id  2:rowest_id  3:version  4:seg_id 
 using MetaDeleteBitmapInfo = BasicKeyInfo<22 , std::tuple<std::string, int64_t,     std::string, int64_t, int64_t>>;
 
+// partition_id of -1 indicates all partitions
 //                                                      0:instance_id  1:table_id 2:partition_id
 using MetaDeleteBitmapUpdateLockInfo = BasicKeyInfo<23 , std::tuple<std::string, int64_t, int64_t>>;
 

cloud/src/meta-service/meta_service.cpp
@@ -1684,9 +1684,7 @@ void MetaServiceImpl::update_delete_bitmap(google::protobuf::RpcController* cont
 
     // 1.Check whether the lock expires
     auto table_id = request->table_id();
-    auto partition_id = request->partition_id();
-    std::string lock_key =
-            meta_delete_bitmap_update_lock_key({instance_id, table_id, partition_id});
+    std::string lock_key = meta_delete_bitmap_update_lock_key({instance_id, table_id, -1});
     std::string lock_val;
     DeleteBitmapUpdateLockPB lock_info;
     ret = txn->get(lock_key, &lock_val);
@@ -1925,66 +1923,60 @@ void MetaServiceImpl::get_delete_bitmap_update_lock(
         return;
     }
     auto table_id = request->table_id();
-    for (size_t i = 0; i < request->partition_ids_size(); i++) {
-        int64_t partition_id = request->partition_ids(i);
-        std::string lock_key =
-                meta_delete_bitmap_update_lock_key({instance_id, table_id, partition_id});
-        std::string lock_val;
-        DeleteBitmapUpdateLockPB lock_info;
-        ret = txn->get(lock_key, &lock_val);
-        if (ret < 0) {
-            ss << "failed to get delete bitmap update lock, instance_id=" << instance_id
-               << " table_id=" << table_id << " key=" << hex(lock_key) << " ret=" << ret;
-            msg = ss.str();
-            code = MetaServiceCode::KV_TXN_GET_ERR;
-            return;
-        }
-        using namespace std::chrono;
-        int64_t now = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
-        if (ret == 0) {
-            if (!lock_info.ParseFromString(lock_val)) [[unlikely]] {
-                code = MetaServiceCode::PROTOBUF_PARSE_ERR;
-                msg = "failed to parse DeleteBitmapUpdateLockPB";
-                return;
-            }
-            if (lock_info.expiration() > 0 && lock_info.expiration() < now) {
-                LOG(INFO) << "delete bitmap lock expired, continue to process. lock_id="
-                          << lock_info.lock_id() << " table_id=" << table_id
-                          << " partition_id=" << partition_id << " now=" << now;
-                lock_info.clear_initiators();
-            } else if (lock_info.lock_id() != request->lock_id()) {
-                ss << "already be locked. requset lock_id=" << request->lock_id()
-                   << " locked by lock_id=" << lock_info.lock_id() << " table_id=" << table_id
-                   << " partition_id=" << partition_id << " now=" << now
-                   << " expiration=" << lock_info.expiration();
-                msg = ss.str();
-                code = MetaServiceCode::LOCK_CONFLICT;
-                continue;
-            }
-        }
-
-        lock_info.set_lock_id(request->lock_id());
-        lock_info.set_expiration(now + request->expiration());
-        bool found = false;
-        for (auto initiator : lock_info.initiators()) {
-            if (request->initiator() == initiator) {
-                found = true;
-                break;
-            }
-        }
-        if (!found) {
-            lock_info.add_initiators(request->initiator());
-        }
-        lock_info.SerializeToString(&lock_val);
-        if (lock_val.empty()) {
-            code = MetaServiceCode::PROTOBUF_SERIALIZE_ERR;
-            msg = "pb serialization error";
-            return;
-        }
-        txn->put(lock_key, lock_val);
-        LOG(INFO) << "xxx put lock_key=" << hex(lock_key) << " lock_id=" << request->lock_id()
-                  << " initiators_size: " << lock_info.initiators_size();
+    std::string lock_key = meta_delete_bitmap_update_lock_key({instance_id, table_id, -1});
+    std::string lock_val;
+    DeleteBitmapUpdateLockPB lock_info;
+    ret = txn->get(lock_key, &lock_val);
+    if (ret < 0) {
+        ss << "failed to get delete bitmap update lock, instance_id=" << instance_id
+           << " table_id=" << table_id << " key=" << hex(lock_key) << " ret=" << ret;
+        msg = ss.str();
+        code = MetaServiceCode::KV_TXN_GET_ERR;
+        return;
     }
+    using namespace std::chrono;
+    int64_t now = duration_cast<seconds>(system_clock::now().time_since_epoch()).count();
+    if (ret == 0) {
+        if (!lock_info.ParseFromString(lock_val)) [[unlikely]] {
+            code = MetaServiceCode::PROTOBUF_PARSE_ERR;
+            msg = "failed to parse DeleteBitmapUpdateLockPB";
+            return;
+        }
+        if (lock_info.expiration() > 0 && lock_info.expiration() < now) {
+            LOG(INFO) << "delete bitmap lock expired, continue to process. lock_id="
+                      << lock_info.lock_id() << " table_id=" << table_id << " now=" << now;
+            lock_info.clear_initiators();
+        } else if (lock_info.lock_id() != request->lock_id()) {
+            ss << "already be locked. requset lock_id=" << request->lock_id()
+               << " locked by lock_id=" << lock_info.lock_id() << " table_id=" << table_id
+               << " now=" << now << " expiration=" << lock_info.expiration();
+            msg = ss.str();
+            code = MetaServiceCode::LOCK_CONFLICT;
+            return;
+        }
+    }
+
+    lock_info.set_lock_id(request->lock_id());
+    lock_info.set_expiration(now + request->expiration());
+    bool found = false;
+    for (auto initiator : lock_info.initiators()) {
+        if (request->initiator() == initiator) {
+            found = true;
+            break;
+        }
+    }
+    if (!found) {
+        lock_info.add_initiators(request->initiator());
+    }
+    lock_info.SerializeToString(&lock_val);
+    if (lock_val.empty()) {
+        code = MetaServiceCode::PROTOBUF_SERIALIZE_ERR;
+        msg = "pb serialization error";
+        return;
+    }
+    txn->put(lock_key, lock_val);
+    LOG(INFO) << "xxx put lock_key=" << hex(lock_key) << " lock_id=" << request->lock_id()
+              << " initiators_size: " << lock_info.initiators_size();
 
     ret = txn->commit();
     if (ret != 0) {

cloud/src/meta-service/meta_service_job.cpp
@@ -68,6 +68,9 @@ static inline constexpr size_t get_file_name_offset(const T (&s)[S], size_t i = 
 
 namespace selectdb {
 
+static constexpr int COMPACTION_DELETE_BITMAP_LOCK_ID = -1;
+static constexpr int SCHEMA_CHANGE_DELETE_BITMAP_LOCK_ID = -2;
+
 extern std::string get_instance_id(const std::shared_ptr<ResourceManager>& rc_mgr,
                                    const std::string& cloud_unique_id);
 
@@ -571,11 +574,11 @@ void process_compaction_job(MetaServiceCode& code, std::string& msg, std::string
     bool enable_unique_key_merge_on_write = compaction.has_delete_bitmap_lock_initiator();
     if (enable_unique_key_merge_on_write) {
         std::string lock_key =
-                meta_delete_bitmap_update_lock_key({instance_id, table_id, partition_id});
+                meta_delete_bitmap_update_lock_key({instance_id, table_id, -1});
         std::string lock_val;
         ret = txn->get(lock_key, &lock_val);
         LOG(INFO) << "get delete bitmap update lock info, table_id=" << table_id
-                  << " partition_id=" << partition_id << " key=" << hex(lock_key) << " ret=" << ret;
+                  << " key=" << hex(lock_key) << " ret=" << ret;
         if (ret != 0) {
             ss << "failed to get delete bitmap update lock key, instance_id=" << instance_id
                << " table_id=" << table_id << " key=" << hex(lock_key) << " ret=" << ret;
@@ -589,7 +592,7 @@ void process_compaction_job(MetaServiceCode& code, std::string& msg, std::string
             msg = "failed to parse DeleteBitmapUpdateLockPB";
             return;
         }
-        if (lock_info.lock_id() != -1) {
+        if (lock_info.lock_id() != COMPACTION_DELETE_BITMAP_LOCK_ID) {
             msg = "lock id not match";
             code = MetaServiceCode::LOCK_EXPIRED;
             return;
@@ -1025,12 +1028,11 @@ void process_schema_change_job(MetaServiceCode& code, std::string& msg, std::str
     // process mow table, check lock
     if (new_tablet_meta.enable_unique_key_merge_on_write()) {
         std::string lock_key =
-                meta_delete_bitmap_update_lock_key({instance_id, new_table_id, new_partition_id});
+                meta_delete_bitmap_update_lock_key({instance_id, new_table_id, -1});
         std::string lock_val;
         ret = txn->get(lock_key, &lock_val);
         LOG(INFO) << "get delete bitmap update lock info, table_id=" << new_table_id
-                  << " partition_id=" << new_partition_id << " key=" << hex(lock_key)
-                  << " ret=" << ret;
+                  << " key=" << hex(lock_key) << " ret=" << ret;
         if (ret != 0) {
             ss << "failed to get delete bitmap update lock key, instance_id=" << instance_id
                << " table_id=" << new_table_id << " key=" << hex(lock_key) << " ret=" << ret;
@@ -1044,7 +1046,7 @@ void process_schema_change_job(MetaServiceCode& code, std::string& msg, std::str
             msg = "failed to parse DeleteBitmapUpdateLockPB";
             return;
         }
-        if (lock_info.lock_id() != -2) {
+        if (lock_info.lock_id() != SCHEMA_CHANGE_DELETE_BITMAP_LOCK_ID) {
             msg = "lock id not match";
             code = MetaServiceCode::LOCK_EXPIRED;
             return;

cloud/src/meta-service/meta_service_txn.cpp
@@ -642,7 +642,6 @@ void MetaServiceImpl::commit_txn(::google::protobuf::RpcController* controller,
     std::map<int64_t, TabletStats> tablet_stats; // tablet_id -> stats
     std::map<int64_t, TabletIndexPB> table_ids;  // tablet_id -> {table/index/partition}_id
     std::map<int64_t, std::vector<int64_t>> table_id_tablet_ids; // table_id -> tablets_ids
-    std::map<int64_t, std::vector<int64_t>> table_id_partition_ids;
     rowsets.reserve(tmp_rowsets_meta.size());
     for (auto& [_, i] : tmp_rowsets_meta) {
         int64_t tablet_id = i.tablet_id();
@@ -709,7 +708,6 @@ void MetaServiceImpl::commit_txn(::google::protobuf::RpcController* controller,
             }
             new_version = version + 1;
             new_versions.insert({std::move(ver_key), new_version});
-            table_id_partition_ids[table_id].push_back(partition_id);
         } else {
             new_version = new_versions[ver_key];
         }
@@ -739,36 +737,33 @@ void MetaServiceImpl::commit_txn(::google::protobuf::RpcController* controller,
 
     // process mow table, check lock and remove pending key
     for (auto table_id : request->mow_table_ids()) {
-        for (auto partition_id : table_id_partition_ids[table_id]) {
-            std::string lock_key =
-                    meta_delete_bitmap_update_lock_key({instance_id, table_id, partition_id});
-            std::string lock_val;
-            ret = txn->get(lock_key, &lock_val);
-            LOG(INFO) << "get delete bitmap update lock info, table_id=" << table_id
-                      << " key=" << hex(lock_key) << " ret=" << ret;
-            if (ret != 0) {
-                ss << "failed to get delete bitmap update lock key info, instance_id="
-                   << instance_id << " table_id=" << table_id << " key=" << hex(lock_key)
-                   << " ret=" << ret;
-                msg = ss.str();
-                code = MetaServiceCode::KV_TXN_GET_ERR;
-                return;
-            }
-            DeleteBitmapUpdateLockPB lock_info;
-            if (!lock_info.ParseFromString(lock_val)) [[unlikely]] {
-                code = MetaServiceCode::PROTOBUF_PARSE_ERR;
-                msg = "failed to parse DeleteBitmapUpdateLockPB";
-                return;
-            }
-            if (lock_info.lock_id() != request->txn_id()) {
-                msg = "lock is expired";
-                code = MetaServiceCode::LOCK_EXPIRED;
-                return;
-            }
-            txn->remove(lock_key);
-            LOG(INFO) << "xxx remove delete bitmap lock, lock_key=" << hex(lock_key)
-                      << " txn_id=" << txn_id;
+        std::string lock_key =
+                meta_delete_bitmap_update_lock_key({instance_id, table_id, -1});
+        std::string lock_val;
+        ret = txn->get(lock_key, &lock_val);
+        LOG(INFO) << "get delete bitmap update lock info, table_id=" << table_id
+                  << " key=" << hex(lock_key) << " ret=" << ret;
+        if (ret != 0) {
+            ss << "failed to get delete bitmap update lock key info, instance_id=" << instance_id
+               << " table_id=" << table_id << " key=" << hex(lock_key) << " ret=" << ret;
+            msg = ss.str();
+            code = MetaServiceCode::KV_TXN_GET_ERR;
+            return;
         }
+        DeleteBitmapUpdateLockPB lock_info;
+        if (!lock_info.ParseFromString(lock_val)) [[unlikely]] {
+            code = MetaServiceCode::PROTOBUF_PARSE_ERR;
+            msg = "failed to parse DeleteBitmapUpdateLockPB";
+            return;
+        }
+        if (lock_info.lock_id() != request->txn_id()) {
+            msg = "lock is expired";
+            code = MetaServiceCode::LOCK_EXPIRED;
+            return;
+        }
+        txn->remove(lock_key);
+        LOG(INFO) << "xxx remove delete bitmap lock, lock_key=" << hex(lock_key)
+                  << " txn_id=" << txn_id;
 
         for (auto tablet_id : table_id_tablet_ids[table_id]) {
             std::string pending_key = meta_pending_delete_bitmap_key({instance_id, tablet_id});

commit c3646174b9bd4b8a15514090a59e36388c408ff6

(selectdb-cloud) Fix partial update fail to get delete bitmap lock info introduced by #2602 (#2626)
fix test_primary_key_partial_update_with_update_stmt and test_primary_key_partial_update_with_delete_stmt.
introduced by: #2602

fe/fe-core/src/main/java/org/apache/doris/transaction/CloudGlobalTransactionMgr.java
@@ -270,8 +270,12 @@ public class CloudGlobalTransactionMgr implements GlobalTransactionMgrInterface 
                 .setTxnId(transactionId)
                 .setIs2Pc(is2PC)
                 .setCloudUniqueId(Config.cloud_unique_id);
-        for (OlapTable olapTable : mowTableList) {
-            builder.addMowTableIds(olapTable.getId());
+
+        // if tablet commit info is empty, no need to pass mowTableList to meta service.
+        if (tabletCommitInfos != null && !tabletCommitInfos.isEmpty()) {
+            for (OlapTable olapTable : mowTableList) {
+                builder.addMowTableIds(olapTable.getId());
+            }
         }
 
         if (txnCommitAttachment != null) {