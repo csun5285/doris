csun5285/doris

commit efd293d3cefd788417a6949314ac51829443173d

(selectdb-cloud) Support to disable empty partition prune (#2639)
Add a session variable disable_empty_partition_prune, which is false by default.
set disable_empty_partition_prune = true to skip checking the data version,
which may take a lot of time, when analyzing.

fe/fe-core/src/main/java/com/selectdb/cloud/catalog/CloudPartition.java
@@ -9,6 +9,7 @@ import org.apache.doris.catalog.DistributionInfo;
 import org.apache.doris.catalog.MaterializedIndex;
 import org.apache.doris.catalog.Partition;
 import org.apache.doris.common.Config;
+import org.apache.doris.qe.ConnectContext;
 import org.apache.doris.rpc.RpcException;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -16,6 +17,7 @@ import org.apache.logging.log4j.Logger;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
@@ -82,11 +84,13 @@ public class CloudPartition extends Partition {
                 version = resp.getVersion();
             } else {
                 assert resp.getStatus().getCode() == MetaServiceCode.VERSION_NOT_FOUND;
-                version = 0;
             }
             LOG.debug("get version from meta service, version: {}, partition: {}", version, super.getId());
             // Cache visible version, see hasData() for details.
             super.setVisibleVersion(version);
+            if (version == 0 && isEmptyPartitionPruneDisabled()) {
+                version = 1;
+            }
             return version;
         } catch (RpcException e) {
             throw new RuntimeException("get version from meta service failed");
@@ -126,6 +130,14 @@ public class CloudPartition extends Partition {
         }
 
         LOG.debug("get version from meta service, partitions: {}, versions: {}", partitionIds, versions);
+
+        if (isEmptyPartitionPruneDisabled()) {
+            ArrayList<Long> news = new ArrayList();
+            for (Long v : versions) {
+                news.add(v == -1 ? 1 : v);
+            }
+            return news;
+        }
         return versions;
     }
 
@@ -168,6 +180,14 @@ public class CloudPartition extends Partition {
      */
     @Override
     public boolean hasData() {
+        // In order to determine whether a partition is empty, a get_version RPC is issued to
+        // the meta service. The pruning process will be very slow when there are lots of empty
+        // partitions. This option disables the empty partition prune optimization to speed SQL
+        // analysis/plan phase.
+        if (isEmptyPartitionPruneDisabled()) {
+            return true;
+        }
+
         // Every partition starts from version 1, version 1 has no data
         final long versionNoData = 1;
 
@@ -224,6 +244,14 @@ public class CloudPartition extends Partition {
         LOG.warn("get version from meta service failed after retry {} times",
                 Config.cloud_meta_service_rpc_failed_retry_times);
         throw new RpcException("get version from meta service", "failed after retry n times");
+    }
+
+    private static boolean isEmptyPartitionPruneDisabled() {
+        ConnectContext ctx = ConnectContext.get();
+        if (ctx != null && ctx.getSessionVariable().getDisableEmptyPartitionPrune()) {
+            return true;
+        }
+        return false;
     }
 
     public static CloudPartition read(DataInput in) throws IOException {

fe/fe-core/src/main/java/org/apache/doris/qe/SessionVariable.java
@@ -137,6 +137,7 @@ public class SessionVariable implements Serializable, Writable {
 
     // SELECTDB_CLOUD_BEGIN
     public static final String CLOUD_CLUSTER = "cloud_cluster";
+    public static final String DISABLE_EMPTY_PARTITION_PRUNE = "disable_empty_partition_prune";
     // SELECTDB_CLOUD_END
 
     // Compatible with  mysql
@@ -658,6 +659,8 @@ public class SessionVariable implements Serializable, Writable {
     // SELECTDB_CODE_BEGIN
     @VariableMgr.VarAttr(name = CLOUD_CLUSTER)
     public String cloudCluster = "";
+    @VariableMgr.VarAttr(name = DISABLE_EMPTY_PARTITION_PRUNE)
+    public boolean disableEmptyPartitionPrune = false;
     // SELECTDB_CODE_END
 
     // -1 means unset, BE will use its config value
@@ -1850,6 +1853,14 @@ public class SessionVariable implements Serializable, Writable {
 
     public String setCloudCluster(String cloudCluster) {
         return this.cloudCluster = cloudCluster;
+    }
+
+    public boolean getDisableEmptyPartitionPrune() {
+        return disableEmptyPartitionPrune;
+    }
+
+    public void setDisableEmptyPartitionPrune(boolean val) {
+        disableEmptyPartitionPrune = val;
     }
 
     // SELECTDB_CODE_END

regression-test/data/partition_p2/test_lots_empty_partitions.out
@@ -1,0 +1,6 @@
+-- This file is automatically generated. You should know what you did if you want to edit this
+-- !sql --
+100	c10
+
+-- !sql --
+

regression-test/suites/partition_p2/test_lots_empty_partitions.groovy
@@ -1,0 +1,64 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+suite("test_lots_empty_partitions") {
+    def tbName = "test_lots_empty_partitions"
+
+    def makePartitionsStmt = { partitionNum ->
+        def stmt = ""
+        for (int i = 0; i < partitionNum; i++) {
+            if (i == (partitionNum - 1)) {
+                stmt += """partition `p${i}` values [("${i}"), ("${i + 1}"))"""
+            } else {
+                stmt += """partition `p${i}` values [("${i}"), ("${i + 1}")),\n\t\t\t"""
+            }
+        }
+        return stmt
+    }
+
+    sql """ DROP TABLE IF EXISTS ${tbName} Force;"""
+    sql """ CREATE TABLE IF NOT EXISTS ${tbName} (
+                `c1` bigint(20) not null,
+                `c2` boolean REPLACE_IF_NOT_NULL null,
+                `c3` tinyint(4) REPLACE_IF_NOT_NULL null,
+                `c4` decimalv3(9, 3) REPLACE_IF_NOT_NULL null,
+                `c5` char(36) REPLACE_IF_NOT_NULL null,
+                `c6` date REPLACE_IF_NOT_NULL null,
+                `c7` datetime REPLACE_IF_NOT_NULL null,
+                `c8` varchar(64) REPLACE_IF_NOT_NULL null,
+                `c9` double REPLACE_IF_NOT_NULL null,
+                `c10` string REPLACE_IF_NOT_NULL null
+            ) engine=olap
+            AGGREGATE KEY(`c1`)
+            PARTITION BY RANGE(`c1`)
+                    (
+                        ${makePartitionsStmt(10000)}
+                    )
+            DISTRIBUTED BY HASH(`c1`) BUCKETS 1
+            PROPERTIES (
+                "replication_num" = "1"
+            );
+    """
+
+    sql """ insert into ${tbName} values (100, false, 1, null, "c5", "2023-09-01", null, "c8", 1.1234, "c10");"""
+    sql """ set disable_empty_partition_prune = true; """
+    qt_sql """ select c1,c10 from ${tbName} order by c1 ASC;"""
+    sql """ truncate table ${tbName};"""
+    qt_sql """ select c1,c10 from ${tbName} order by c1 ASC;"""
+    sql """ set disable_empty_partition_prune = false; """
+}
+

commit d65f2e21fe2d003e856644780530d3fec5954b48

(selectdb-cloud) Fix the wrong visible version introduced by #2639 (#2665)

fe/fe-core/src/main/java/com/selectdb/cloud/catalog/CloudPartition.java
@@ -85,6 +85,7 @@ public class CloudPartition extends Partition {
                 version = resp.getVersion();
             } else {
                 assert resp.getStatus().getCode() == MetaServiceCode.VERSION_NOT_FOUND;
+                version = 0;
             }
             LOG.debug("get version from meta service, version: {}, partition: {}", version, super.getId());
             // Cache visible version, see hasData() for details.