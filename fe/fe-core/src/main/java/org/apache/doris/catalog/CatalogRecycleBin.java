csun5285/doris

commit 64946548dfe19ca835eb8aef3b26fcc76d0543e2

(selectdb-cloud) Use RecycleBin to force drop db/table asynchronously (#2675)

fe/fe-core/src/main/java/org/apache/doris/catalog/CatalogRecycleBin.java
@@ -125,7 +125,7 @@ public class CatalogRecycleBin extends MasterDaemon implements Writable {
     }
 
     public synchronized boolean recycleDatabase(Database db, Set<String> tableNames, Set<Long> tableIds,
-                                                boolean isReplay, long replayRecycleTime) {
+                                                boolean isReplay, boolean isForceDrop, long replayRecycleTime) {
         long recycleTime = 0;
         if (idToDatabase.containsKey(db.getId())) {
             LOG.error("db[{}] already in recycle bin.", db.getId());
@@ -138,17 +138,21 @@ public class CatalogRecycleBin extends MasterDaemon implements Writable {
         // recycle db
         RecycleDatabaseInfo databaseInfo = new RecycleDatabaseInfo(db, tableNames, tableIds);
         idToDatabase.put(db.getId(), databaseInfo);
-        if (!isReplay || replayRecycleTime == 0) {
+        if (isForceDrop) {
+            // The 'force drop' database should be recycle immediately.
+            recycleTime = 0;
+        } else if (!isReplay || replayRecycleTime == 0) {
             recycleTime = System.currentTimeMillis();
         } else {
             recycleTime = replayRecycleTime;
         }
         idToRecycleTime.put(db.getId(), recycleTime);
-        LOG.info("recycle db[{}-{}]", db.getId(), db.getFullName());
+        LOG.info("recycle db[{}-{}], is force drop: {}", db.getId(), db.getFullName(), isForceDrop);
         return true;
     }
 
-    public synchronized boolean recycleTable(long dbId, Table table, boolean isReplay, long replayRecycleTime) {
+    public synchronized boolean recycleTable(long dbId, Table table, boolean isReplay,
+                                             boolean isForceDrop, long replayRecycleTime) {
         long recycleTime = 0;
         if (idToTable.containsKey(table.getId())) {
             LOG.error("table[{}] already in recycle bin.", table.getId());
@@ -157,14 +161,17 @@ public class CatalogRecycleBin extends MasterDaemon implements Writable {
 
         // recycle table
         RecycleTableInfo tableInfo = new RecycleTableInfo(dbId, table);
-        if (!isReplay || replayRecycleTime == 0) {
+        if (isForceDrop) {
+            // The 'force drop' table should be recycle immediately.
+            recycleTime = 0;
+        } else if (!isReplay || replayRecycleTime == 0) {
             recycleTime = System.currentTimeMillis();
         } else {
             recycleTime = replayRecycleTime;
         }
         idToRecycleTime.put(table.getId(), recycleTime);
         idToTable.put(table.getId(), tableInfo);
-        LOG.info("recycle table[{}-{}]", table.getId(), table.getName());
+        LOG.info("recycle table[{}-{}], is force drop: {}", table.getId(), table.getName(), isForceDrop);
         return true;
     }
 
@@ -529,7 +536,9 @@ public class CatalogRecycleBin extends MasterDaemon implements Writable {
 
     public synchronized Database recoverDatabase(String dbName, long dbId) throws DdlException {
         RecycleDatabaseInfo dbInfo = null;
-        long recycleTime = -1;
+        // The recycle time of the force dropped tables and databases will be set to zero, use 1 here to
+        // skip these databases and tables.
+        long recycleTime = 1;
         Iterator<Map.Entry<Long, RecycleDatabaseInfo>> iterator = idToDatabase.entrySet().iterator();
         while (iterator.hasNext()) {
             Map.Entry<Long, RecycleDatabaseInfo> entry = iterator.next();
@@ -608,7 +617,9 @@ public class CatalogRecycleBin extends MasterDaemon implements Writable {
                                              String newTableName) throws DdlException {
         // make sure to get db lock
         Table table = null;
-        long recycleTime = -1;
+        // The recycle time of the force dropped tables and databases will be set to zero, use 1 here to
+        // skip these databases and tables.
+        long recycleTime = 1;
         long dbId = db.getId();
         Iterator<Map.Entry<Long, RecycleTableInfo>> iterator = idToTable.entrySet().iterator();
         while (iterator.hasNext()) {

fe/fe-core/src/main/java/org/apache/doris/datasource/InternalCatalog.java
@@ -513,6 +513,7 @@ public class InternalCatalog implements CatalogIf<Database> {
 
     public void dropDb(DropDbStmt stmt) throws DdlException {
         String dbName = stmt.getDbName();
+        LOG.info("begin drop database[{}], is force : {}", dbName, stmt.isForceDrop());
 
         // 1. check if database exists
         if (!tryLock(false)) {
@@ -571,12 +572,8 @@ public class InternalCatalog implements CatalogIf<Database> {
                     MetaLockUtils.writeUnlockTables(tableList);
                 }
 
-                if (!stmt.isForceDrop()) {
-                    Env.getCurrentRecycleBin().recycleDatabase(db, tableNames, tableIds, false, 0);
-                    recycleTime = Env.getCurrentRecycleBin().getRecycleTimeById(db.getId());
-                } else {
-                    Env.getCurrentEnv().eraseDatabase(db.getId(), false);
-                }
+                Env.getCurrentRecycleBin().recycleDatabase(db, tableNames, tableIds, false, stmt.isForceDrop(), 0);
+                recycleTime = Env.getCurrentRecycleBin().getRecycleTimeById(db.getId());
             } finally {
                 db.writeUnlock();
             }
@@ -623,11 +620,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                 } finally {
                     MetaLockUtils.writeUnlockTables(tableList);
                 }
-                if (!isForceDrop) {
-                    Env.getCurrentRecycleBin().recycleDatabase(db, tableNames, tableIds, true, recycleTime);
-                } else {
-                    Env.getCurrentEnv().eraseDatabase(db.getId(), false);
-                }
+                Env.getCurrentRecycleBin().recycleDatabase(db, tableNames, tableIds, true, isForceDrop, recycleTime);
                 Env.getCurrentEnv().getQueryStats().clear(Env.getCurrentEnv().getInternalCatalog().getId(), db.getId());
             } finally {
                 db.writeUnlock();
@@ -896,6 +889,7 @@ public class InternalCatalog implements CatalogIf<Database> {
     public void dropTable(DropTableStmt stmt) throws DdlException {
         String dbName = stmt.getDbName();
         String tableName = stmt.getTableName();
+        LOG.info("begin to drop table: {} from db: {}, is force: {}", tableName, dbName, stmt.isForceDrop());
 
         // check database
         Database db = (Database) getDbOrDdlException(dbName);
@@ -998,14 +992,7 @@ public class InternalCatalog implements CatalogIf<Database> {
                 MetricRepo.DORIS_METRIC_REGISTER.removeMetricsByNameAndLabels(metric.getName(), metric.getLabels());
             }
         }
-        if (!isForceDrop) {
-            Env.getCurrentRecycleBin().recycleTable(db.getId(), table, isReplay, recycleTime);
-        } else {
-            if (table.getType() == TableType.OLAP) {
-                Env.getCurrentEnv().onEraseOlapTable((OlapTable) table, isReplay);
-            }
-        }
-
+        Env.getCurrentRecycleBin().recycleTable(db.getId(), table, isReplay, isForceDrop, recycleTime);
         if (table instanceof MaterializedView) {
             List<Long> dropIds = Env.getCurrentEnv().getMTMVJobManager().showJobs(db.getFullName(), table.getName())
                     .stream().map(MTMVJob::getId).collect(Collectors.toList());

regression-test/suites/ddl_p0/test_drop_force.groovy
@@ -1,0 +1,75 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+// this suite is for creating table with timestamp datatype in defferent 
+// case. For example: 'year' and 'Year' datatype should also be valid in definition
+
+suite("sql_force_drop") {
+    def testTable = "test_force_drop"
+
+    sql "CREATE DATABASE IF NOT EXISTS test_force_drop_database"
+    sql """
+        CREATE TABLE IF NOT EXISTS test_force_drop_database.table1 (
+            `actorid` varchar(128),
+            `gameid` varchar(128),
+            `eventtime` datetimev2(3)
+        )
+        engine=olap
+        duplicate key(actorid, gameid, eventtime)
+        partition by range(eventtime)(
+            from ("2000-01-01") to ("2021-01-01") interval 1 year,
+            from ("2021-01-01") to ("2022-01-01") interval 1 MONth,
+            from ("2022-01-01") to ("2023-01-01") interval 1 WEEK,
+            from ("2023-01-01") TO ("2023-02-01") interval 1 DAY
+        )
+        distributed by hash(actorid) buckets 1
+        properties(
+            "replication_num"="1",
+            "light_schema_change"="true",
+            "compression"="zstd"
+        );
+    """"
+    sql """
+        CREATE TABLE IF NOT EXISTS test_force_drop_database.table2 (
+            `actorid` varchar(128),
+            `gameid` varchar(128),
+            `eventtime` datetimev2(3)
+        )
+        engine=olap
+        duplicate key(actorid, gameid, eventtime)
+        partition by range(eventtime)(
+            from ("2000-01-01") to ("2021-01-01") interval 1 year,
+            from ("2021-01-01") to ("2022-01-01") interval 1 MONth,
+            from ("2022-01-01") to ("2023-01-01") interval 1 WEEK,
+            from ("2023-01-01") TO ("2023-02-01") interval 1 DAY
+        )
+        distributed by hash(actorid) buckets 1
+        properties(
+            "replication_num"="1",
+            "light_schema_change"="true",
+            "compression"="zstd"
+        );
+    """"
+
+    sql " drop table test_force_drop_database.table2 "
+    sql " recover table test_force_drop_database.table2 "
+    sql " drop table test_force_drop_database.table2 FORCE"
+    sql " drop database test_force_drop_database "
+    sql " recover database test_force_drop_database "
+    sql " drop database test_force_drop_database FORCE"
+}
+

commit 8fc2e8a21b750ad42fbf9f9f1a344a5f93212a9e

(selectdb-cloud) Fix FE null pointer exception introduced by #2675 when replay recycle bin (#2706)
which caused by replaying erase table before drop db

fe/fe-core/src/main/java/org/apache/doris/catalog/CatalogRecycleBin.java
@@ -416,6 +416,11 @@ public class CatalogRecycleBin extends MasterDaemon implements Writable {
         LOG.info("before replay erase table[{}]", tableId);
         RecycleTableInfo tableInfo = idToTable.remove(tableId);
         idToRecycleTime.remove(tableId);
+        if (table == null) {
+            // FIXME(walter): Sometimes `eraseTable` in 'DROP DB ... FORCE' may be executed earlier than
+            // finish drop db, especially in the case of drop db with many tables.
+            return;
+        }
         Table table = tableInfo.getTable();
         if (table.getType() == TableType.OLAP && !Env.isCheckpointThread()) {
             Env.getCurrentEnv().onEraseOlapTable((OlapTable) table, true);