91d63e4b090b4128e5f3d33a38cea1894e19d484
Lei Zhang <27994433+SWJTU-ZhangLei@users.noreply.github.com>
2023-10-18 13:13:56 +0800

    (selectdb-cloud) Disable query table rowsets and backends of information_schema (#1374) (#2723)
    * Pick 00342279171abb929f29398fcdcee510aca50397 again
    * http://jira.selectdb-in.cc/browse/CIR-4939

---
+++ regression-test/suites/cloud/auth/test_disable_query_information_schema.groovy
@@ -0,0 +1,30 @@
+suite("test_disable_query_information_schema", "cloud_auth") {
+    def user1 = "information_schema_user"
+    sql """drop user if exists ${user1}"""
+
+    sql """create user ${user1} identified by '12345' default role 'admin'"""
+
+    try {
+        result = connect(user = "${user1}", password = '12345', url = context.config.jdbcUrl) {
+             sql "set enable_nereids_planner = false"
+             sql """
+                select * from information_schema.rowsets;
+             """
+        }
+    } catch (Exception e) {
+        assertTrue(e.getMessage().contains("Unsupported operation"), e.getMessage())
+    }
+
+    try {
+        result = connect(user = "${user1}", password = '12345', url = context.config.jdbcUrl) {
+             sql "set enable_nereids_planner = true"
+             sql """
+                select * from information_schema.rowsets;
+             """
+        }
+    } catch (Exception e) {
+        assertTrue(e.getMessage().contains("Unsupported operation"), e.getMessage())
+    }
+
+    sql """drop user if exists ${user1}"""
+}
