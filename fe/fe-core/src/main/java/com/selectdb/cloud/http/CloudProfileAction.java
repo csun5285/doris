2b982a4a743ea4fd3441c85d4b96f5b68c43e4da
Luwei <814383175@qq.com>
2023-10-25 22:12:24 +0800

    (selectdb-cloud) Add schema and profile http api for the cloud http port (#2761)

---
+++ fe/fe-core/src/main/java/com/selectdb/cloud/http/CloudProfileAction.java
@@ -0,0 +1,39 @@
+// Licensed to the Apache Software Foundation (ASF) under one
+// or more contributor license agreements.  See the NOTICE file
+// distributed with this work for additional information
+// regarding copyright ownership.  The ASF licenses this file
+// to you under the Apache License, Version 2.0 (the
+// "License"); you may not use this file except in compliance
+// with the License.  You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing,
+// software distributed under the License is distributed on an
+// "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+// KIND, either express or implied.  See the License for the
+// specific language governing permissions and limitations
+// under the License.
+
+package com.selectdb.cloud.http;
+
+import org.apache.doris.httpv2.rest.ProfileAction;
+import org.apache.doris.mysql.privilege.PrivPredicate;
+import org.apache.doris.qe.ConnectContext;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@RestController
+public class CloudProfileAction extends ProfileAction {
+
+    @RequestMapping(path = "/api/profile", method = RequestMethod.GET)
+    protected Object profile(HttpServletRequest request, HttpServletResponse response) {
+        executeCheckPassword(request, response);
+        checkGlobalAuth(ConnectContext.get().getCurrentUserIdentity(), PrivPredicate.ADMIN);
+        return super.profile(request, response);
+    }
+}
