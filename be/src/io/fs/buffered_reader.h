9960f0bb6bbe4927ee66385695ab42afb995ac8b
AlexYue <yj976240184@gmail.com>
2023-11-14 23:15:09 +0800

    (selectdb-cloud) Do not use profile for merge range reader, print log instead (#2843)
    http://39.106.86.136:8090/browse/CORE-3018

--- be/src/io/fs/buffered_reader.h
+++ be/src/io/fs/buffered_reader.h
@@ -168,20 +168,26 @@ public:
 
     Status close() override {
         if (!_closed) {
+            
             _closed = true;
             // the underlying buffer is closed in its own destructor
             // return _reader->close();
-            if (_profile != nullptr) {
-                // Make the thread sleep for a while to test data race
-                TEST_INJECTION_POINT_CALLBACK("MergeRangeFileReader::close");
-                COUNTER_UPDATE(_copy_time, _statistics.copy_time);
-                COUNTER_UPDATE(_read_time, _statistics.read_time);
-                COUNTER_UPDATE(_request_io, _statistics.request_io);
-                COUNTER_UPDATE(_merged_io, _statistics.merged_io);
-                COUNTER_UPDATE(_request_bytes, _statistics.request_bytes);
-                COUNTER_UPDATE(_read_bytes, _statistics.read_bytes);
-            }
-            _profile = nullptr;
+            // Fixme(AlexYue): Temporiraly comment the profile update logic
+            // if (_profile != nullptr) {
+            //     COUNTER_UPDATE(_copy_time, _statistics.copy_time);
+            //     COUNTER_UPDATE(_read_time, _statistics.read_time);
+            //     COUNTER_UPDATE(_request_io, _statistics.request_io);
+            //     COUNTER_UPDATE(_merged_io, _statistics.merged_io);
+            //     COUNTER_UPDATE(_request_bytes, _statistics.request_bytes);
+            //     COUNTER_UPDATE(_read_bytes, _statistics.read_bytes);
+            // }
+            // _profile = nullptr;
+            LOG_INFO(
+                    "The merge range reader's profile, path {}, copy time {}, read time {}, "
+                    "request io {}, merged io {}, request bytes {}, merged bytes {}",
+                    _reader->path().string(), _statistics.copy_time, _statistics.read_time,
+                    _statistics.request_io, _statistics.merged_io, _statistics.request_bytes,
+                    _statistics.read_bytes);
         }
         return Status::OK();
     }
