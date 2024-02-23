7132feccfcb52036ed1b46d0502ddcb1f1204e9c
zy-kkk <zhongyk10@gmail.com>
2024-02-23 18:01:18 +0800

    [pick](jdbc catalog) pick two jdbc catalog mysql function push down role #30778 ##30882 (#3154)
    * [improvement](catalog) fix jdbc mysql catalog to_date fun pushdown (#29900) (#30778)
    * [pick][fix](catalog) fix data_sub/data_add func pushdown in jdbcscan (#30882)

--- fe/fe-core/src/main/java/org/apache/doris/planner/external/jdbc/JdbcFunctionPushDownRule.java
+++ fe/fe-core/src/main/java/org/apache/doris/planner/external/jdbc/JdbcFunctionPushDownRule.java
@@ -20,6 +20,7 @@ package org.apache.doris.planner.external.jdbc;
 import org.apache.doris.analysis.Expr;
 import org.apache.doris.analysis.FunctionCallExpr;
 import org.apache.doris.analysis.FunctionName;
+import org.apache.doris.analysis.TimestampArithmeticExpr;
 import org.apache.doris.thrift.TOdbcTableType;
 
 import com.google.common.base.Preconditions;
@@ -61,10 +62,7 @@ public class JdbcFunctionPushDownRule {
 
     static {
         REPLACE_MYSQL_FUNCTIONS.put("nvl", "ifnull");
-    }
-
-    private static boolean isReplaceMysqlFunctions(String functionName) {
-        return REPLACE_MYSQL_FUNCTIONS.containsKey(functionName.toLowerCase());
+        REPLACE_MYSQL_FUNCTIONS.put("to_date", "date");
     }
 
     private static final Map<String, String> REPLACE_CLICKHOUSE_FUNCTIONS = Maps.newHashMap();
@@ -72,6 +70,10 @@ public class JdbcFunctionPushDownRule {
     static {
         REPLACE_CLICKHOUSE_FUNCTIONS.put("from_unixtime", "FROM_UNIXTIME");
         REPLACE_CLICKHOUSE_FUNCTIONS.put("unix_timestamp", "toUnixTimestamp");
+    }
+
+    private static boolean isReplaceMysqlFunctions(String functionName) {
+        return REPLACE_MYSQL_FUNCTIONS.containsKey(functionName.toLowerCase());
     }
 
     private static boolean isReplaceClickHouseFunctions(String functionName) {
@@ -115,6 +117,8 @@ public class JdbcFunctionPushDownRule {
             }
 
             replaceFunctionNameIfNecessary(func, replaceFunction, functionCallExpr, tableType);
+
+            expr = replaceGenericFunctionExpr(functionCallExpr, func);
         }
 
         List<Expr> children = expr.getChildren();
@@ -127,7 +131,7 @@ public class JdbcFunctionPushDownRule {
         return expr;
     }
 
-    private static String replaceFunctionNameIfNecessary(String func, Predicate<String> replaceFunction,
+    private static void replaceFunctionNameIfNecessary(String func, Predicate<String> replaceFunction,
             FunctionCallExpr functionCallExpr, TOdbcTableType tableType) {
         if (replaceFunction.test(func)) {
             String newFunc;
@@ -140,9 +144,48 @@ public class JdbcFunctionPushDownRule {
             }
             if (newFunc != null) {
                 functionCallExpr.setFnName(FunctionName.createBuiltinName(newFunc));
-                func = functionCallExpr.getFnName().getFunction();
             }
         }
-        return func;
+    }
+
+    // Function used to convert nereids planner's function to old planner's function
+    private static Expr replaceGenericFunctionExpr(FunctionCallExpr functionCallExpr, String func) {
+        Map<String, String> supportedTimeUnits = Maps.newHashMap();
+        supportedTimeUnits.put("years", "YEAR");
+        supportedTimeUnits.put("months", "MONTH");
+        supportedTimeUnits.put("weeks", "WEEK");
+        supportedTimeUnits.put("days", "DAY");
+        supportedTimeUnits.put("hours", "HOUR");
+        supportedTimeUnits.put("minutes", "MINUTE");
+        supportedTimeUnits.put("seconds", "SECOND");
+
+        String baseFuncName = null;
+        String timeUnit = null;
+
+        for (Map.Entry<String, String> entry : supportedTimeUnits.entrySet()) {
+            if (func.endsWith(entry.getKey() + "_add")) {
+                baseFuncName = "date_add";
+                timeUnit = entry.getValue();
+                break;
+            } else if (func.endsWith(entry.getKey() + "_sub")) {
+                baseFuncName = "date_sub";
+                timeUnit = entry.getValue();
+                break;
+            }
+        }
+
+        if (baseFuncName != null && timeUnit != null) {
+            if (functionCallExpr.getChildren().size() == 2) {
+                Expr child1 = functionCallExpr.getChild(0);
+                Expr child2 = functionCallExpr.getChild(1);
+                return new TimestampArithmeticExpr(
+                        baseFuncName,
+                        child1,
+                        child2,
+                        timeUnit
+                );
+            }
+        }
+        return functionCallExpr;
     }
 }
--- regression-test/suites/external_table_p0/jdbc/test_mysql_jdbc_catalog.groovy
+++ regression-test/suites/external_table_p0/jdbc/test_mysql_jdbc_catalog.groovy
@@ -177,6 +177,10 @@ suite("test_mysql_jdbc_catalog", "p0,external,mysql,external_docker,external_doc
             order_qt_test_dz """select * from ${test_zd} order by 1; """
             order_qt_test_filter_not """select * from ${ex_tb13} where name not like '%张三0%' order by 1; """
             order_qt_test_filter_not_old_plan """select /*+ SET_VAR(enable_nereids_planner=false) */ * from ${ex_tb13} where name not like '%张三0%' order by 1; """
+            explain {
+                sql("select `datetime` from all_types where to_date(`datetime`) = '2012-10-25';")
+                contains """ SELECT `datetime` FROM `doris_test`.`all_types` WHERE (date(`datetime`) = '2012-10-25')"""
+            }
 
             // test insert
             String uuid1 = UUID.randomUUID().toString();
@@ -409,6 +413,94 @@ suite("test_mysql_jdbc_catalog", "p0,external,mysql,external_docker,external_doc
                 contains "QUERY: SELECT `k6`, `k8` FROM `doris_test`.`test1` WHERE (`k8` = 1)"
             }
             sql """ admin set frontend config ("enable_func_pushdown" = "true"); """
+            // test date_add
+            sql """ set disable_nereids_rules='NORMALIZE_REWRITE_RULES'; """
+            order_qt_date_add_year """ select * from test_zd where date_add(d_z,interval 1 year) = '2023-01-01' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 year) = '2023-01-01' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 YEAR) = '2023-01-01')"
+            }
+            order_qt_date_add_month """ select * from test_zd where date_add(d_z,interval 1 month) = '2022-02-01' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 month) = '2022-02-01' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 MONTH) = '2022-02-01')"
+            }
+            order_qt_date_add_week """ select * from test_zd where date_add(d_z,interval 1 week) = '2022-01-08' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 week) = '2022-01-08' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 WEEK) = '2022-01-08')"
+            }
+            order_qt_date_add_day """ select * from test_zd where date_add(d_z,interval 1 day) = '2022-01-02' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 day) = '2022-01-02' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 DAY) = '2022-01-02')"
+            }
+            order_qt_date_add_hour """ select * from test_zd where date_add(d_z,interval 1 hour) = '2022-01-01 01:00:00' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 hour) = '2022-01-01 01:00:00' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 HOUR) = '2022-01-01 01:00:00')"
+            }
+            order_qt_date_add_min """ select * from test_zd where date_add(d_z,interval 1 minute) = '2022-01-01 00:01:00' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 minute) = '2022-01-01 00:01:00' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 MINUTE) = '2022-01-01 00:01:00')"
+            }
+            order_qt_date_add_sec """ select * from test_zd where date_add(d_z,interval 1 second) = '2022-01-01 00:00:01' order by 1; """
+            explain {
+                sql("select * from test_zd where date_add(d_z,interval 1 second) = '2022-01-01 00:00:01' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_add(`d_z`, INTERVAL 1 SECOND) = '2022-01-01 00:00:01')"
+            }
+            // date_sub
+            order_qt_date_sub_year """ select * from test_zd where date_sub(d_z,interval 1 year) = '2021-01-01' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 year) = '2021-01-01' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 YEAR) = '2021-01-01')"
+            }
+            order_qt_date_sub_month """ select * from test_zd where date_sub(d_z,interval 1 month) = '2021-12-01' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 month) = '2021-12-01' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 MONTH) = '2021-12-01')"
+            }
+            order_qt_date_sub_week """ select * from test_zd where date_sub(d_z,interval 1 week) = '2021-12-25' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 week) = '2021-12-25' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 WEEK) = '2021-12-25')"
+            }
+            order_qt_date_sub_day """ select * from test_zd where date_sub(d_z,interval 1 day) = '2021-12-31' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 day) = '2021-12-31' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 DAY) = '2021-12-31')"
+            }
+            order_qt_date_sub_hour """ select * from test_zd where date_sub(d_z,interval 1 hour) = '2021-12-31 23:00:00' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 hour) = '2021-12-31 23:00:00' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 HOUR) = '2021-12-31 23:00:00')"
+            }
+            order_qt_date_sub_min """ select * from test_zd where date_sub(d_z,interval 1 minute) = '2021-12-31 23:59:00' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 minute) = '2021-12-31 23:59:00' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 MINUTE) = '2021-12-31 23:59:00')"
+            }
+            order_qt_date_sub_sec """ select * from test_zd where date_sub(d_z,interval 1 second) = '2021-12-31 23:59:59' order by 1; """
+            explain {
+                sql("select * from test_zd where date_sub(d_z,interval 1 second) = '2021-12-31 23:59:59' order by 1;")
+
+                contains " QUERY: SELECT `id`, `d_z` FROM `doris_test`.`test_zd` WHERE (date_sub(`d_z`, INTERVAL 1 SECOND) = '2021-12-31 23:59:59')"
+            }
+            sql """ set disable_nereids_rules=''; """
         } finally {
 			res_dbs_log = sql "show databases;"
 			for(int i = 0;i < res_dbs_log.size();i++) {
