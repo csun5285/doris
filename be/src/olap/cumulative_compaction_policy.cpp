333362021c8f24d11fb8d3a289a26f7571ed6132
huanghaibin <284824253@qq.com>
2024-04-25 01:30:05 +0800

    (selectdb-cloud) consider version count on size-based cloud cu compaction policy (#3307)
    cloud cu compaction 可以按version数进行compaction

--- be/src/olap/cumulative_compaction_policy.cpp
+++ be/src/olap/cumulative_compaction_policy.cpp
@@ -395,11 +395,20 @@ int64_t SizeBasedCumulativeCompactionPolicy::new_cumulative_point(
         int64_t last_cumulative_point) {
     TEST_INJECTION_POINT_RETURN_WITH_VALUE("new_cumulative_point", int64_t(0), output_rowset.get(),
                                            last_cumulative_point);
+    // for MoW table, if there's too many versions, the delete bitmap will grow to
+    // a very big size, which may cause the tablet meta too big and the `save_meta`
+    // operation too slow.
+    // if the rowset should not promotion according to it's disk size, we should also
+    // consider it's version count here.
+    bool satisfy_promotion_version = tablet->enable_unique_key_merge_on_write() &&
+                                     output_rowset->end_version() - output_rowset->start_version() >
+                                             _promotion_version_count;
     // if rowsets have delete version, move to the last directly.
     // if rowsets have no delete version, check output_rowset total disk size satisfies promotion size.
     return output_rowset->start_version() == last_cumulative_point &&
                            (last_delete_version.first != -1 ||
-                            output_rowset->data_disk_size() >= cloud_promotion_size(tablet))
+                            output_rowset->data_disk_size() >= cloud_promotion_size(tablet) ||
+                            satisfy_promotion_version)
                    ? output_rowset->end_version() + 1
                    : last_cumulative_point;
 }
