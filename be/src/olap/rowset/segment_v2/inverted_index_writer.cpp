cf5e0df6c9b6fec30aaed0626a2b65d77a9b6ff8
airborne12 <airborne08@gmail.com>
2025-07-29 02:36:07 +0800

    [opt](inverted index) create non-analyzer when parser is none (#4226)

--- be/src/olap/rowset/segment_v2/inverted_index_writer.cpp
+++ be/src/olap/rowset/segment_v2/inverted_index_writer.cpp
@@ -248,7 +248,7 @@ public:
         RETURN_IF_ERROR(open_index_directory());
         _char_string_reader =
                 DORIS_TRY(create_char_string_reader(_inverted_index_ctx->char_filter_map));
-        if (_parser_type != InvertedIndexParserType::PARSER_NONE) {
+        if (_should_analyzer) {
             _analyzer = DORIS_TRY(create_analyzer(_inverted_index_ctx));
         }
         _index_writer = create_index_writer();
